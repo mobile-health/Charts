97a1b8484e74a24ca791d7f5d025cef65df7511a
thuongdao <thuongdaovan@gmail.com>
2018-06-18 12:09:39 +0700

    fix bug crash when rendering data

--- Source/Charts/Renderers/ChartDataRendererBase.swift
+++ Source/Charts/Renderers/ChartDataRendererBase.swift
@@ -55,6 +55,6 @@ open class DataRenderer: Renderer
     {
         guard let data = dataProvider?.data
             else { return false }
-        return data.entryCount < Int(CGFloat(dataProvider?.maxVisibleCount ?? 0) * (self.viewPortHandler?.scaleX ?? 1.0))
+        return CGFloat(data.entryCount) < CGFloat(dataProvider?.maxVisibleCount ?? 0) * (self.viewPortHandler?.scaleX ?? 1.0)
     }
 }
